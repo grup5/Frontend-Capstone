3a8c5fcba3d59bbd1680ab27dbb4e0c2052aba82
nateykliu <nathanykliu@gmail.com>
2023-12-22 08:10:56 -0800

    files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-
+import Footer from "./components/Footer";
+import StickyFooter from "./components/StickyFooter";
 
 function App() {
   
   return (
     <>
-      
+      <Footer />
+      <StickyFooter />
     </>
   )
 }
