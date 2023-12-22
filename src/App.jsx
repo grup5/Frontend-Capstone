2e6634e5a656e1fccc5f0c586260aa7ed4124ad2
guy-jerome <aaronjamesroberts@gmail.com>
2023-12-22 11:16:46 -0600

    Added the Recommended Products Carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import RelatedProducts from "./components/RelatedProducts/RelatedProducts.jsx"
-
+import RecommendedProducts from "./components/RecommendedProducts/RecommendedProducts.jsx"
 function App() {
   
   return (
     <>
       <RelatedProducts/>
+      <RecommendedProducts/>
     </>
   )
 }
---
+++ src/components/RecommendedProducts/RecommendedProducts.jsx
@@ -0,0 +1,3 @@
+export default function RecommendedProducts(){
+  return <h1>Hello World</h1>
+}
