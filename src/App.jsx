d5314fdba0b1a3e8717179550e6025cc95d8581c
NOAH MORRISON <morrison.noah@gmail.com>
2024-01-02 15:10:05 -0800

    Main image commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import Footer from "./components/Footer";
-import StickyFooter from "./components/StickyFooter";
+// import Footer from "./components/Footer";
+// import StickyFooter from "./components/StickyFooter";
+import MainProduct from "./components/imgcontent/MainProduct.jsx"
 
 function App() {
+  const images = ["./img/beanie2.jpg", "./img/beanie3.jpg", "./img/beanie4.jpg", "./img/beanie5.jpg", "./img/beanie6.jpg"]
   
   return (
     <>
-      <Footer />
-      <StickyFooter />
+      <MainProduct images={images}/>
+      {/* <Footer />
+      <StickyFooter /> */}
     </>
   )
 }
