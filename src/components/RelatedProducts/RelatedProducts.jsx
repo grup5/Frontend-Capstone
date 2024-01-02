26621cb1152aaa1bc21192aa9b08d9cfa58d1d98
guy-jerome <aaronjamesroberts@gmail.com>
2024-01-02 14:19:57 -0600

    Created Pop Ups for the carousel

--- src/components/RelatedProducts/RelatedProducts.jsx
+++ src/components/RelatedProducts/RelatedProducts.jsx
@@ -63,6 +63,12 @@ export default function RelatedProducts() {
             onMouseEnter={() => setHoveredProductId(product.id)}
             onMouseLeave={() => setHoveredProductId(null)}
           >
+            {
+              hoveredProductId === product.id&&<div className='new-tag'>NEW</div>
+            }
+            {
+              hoveredProductId === product.id&&<div style={{backgroundColor:product.color}}className='detail-tag'><h6>{product.name}</h6><p>{product.priceString}</p></div>
+            }
             <img
               src={hoveredProductId === product.id ? product.imageUrlBack : product.imageUrlFront}
               alt={product.name}
