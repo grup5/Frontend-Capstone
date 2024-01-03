2f6e0159ce0951fc878589088bf5c669663158fe
NOAH MORRISON <morrison.noah@gmail.com>
2024-01-03 07:41:34 -0800

    Body CSS Fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,23 @@ import Description from './components/description/description'
 import MainProduct from "./components/imgcontent/MainProduct.jsx"
 import RelatedProducts from "./components/RelatedProducts/RelatedProducts.jsx"
 import RecommendedProducts from "./components/RecommendedProducts/RecommendedProducts.jsx"
-
+import NavBar from "./components/Header/NavBar.jsx"
+import "./style.css"
 
 function App() {
   const images = ["./img/beanie2.jpg", "./img/beanie3.jpg", "./img/beanie4.jpg", "./img/beanie5.jpg", "./img/beanie6.jpg"]
   
   return (
     <>
-      <Description Description={Description}/>
-      <MainProduct images={images}/>
+    <NavBar/>
+      <div className="bodyclass">
+        <MainProduct images={images}/>
+        <Description Description={Description}/>
+      </div>
       <RelatedProducts/>
       <RecommendedProducts/>
+      <Footer />
+      <StickyFooter/>
     </>
   )
 }
