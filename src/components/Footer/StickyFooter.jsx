37a542e44d7b7b04b4f332d95c84a3fc8d2bb7a8
nateykliu <nathanykliu@gmail.com>
2024-01-04 15:03:37 -0800

    new

--- src/components/Footer/StickyFooter.jsx
+++ src/components/Footer/StickyFooter.jsx
@@ -1,10 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import './StickyFooter.css';
+import redbeanie from '/img/footerimages/redbeanie.webp';
+import yellowbeanie from '/img/footerimages/yellowbeanie.webp';
+import bluebeanie from '/img/footerimages/bluebeanie.webp';
+import greenbeanie from '/img/footerimages/greenbeanie.webp';
 
 const StickyFooter = () => {
     const [isVisible, setIsVisible] = useState(false);
     const [quantity, setQuantity] = useState(1);
     const [style, setStyle] = useState('');
+
+    const beanieImages = {
+        red: redbeanie,
+        yellow: yellowbeanie,
+        blue: bluebeanie,
+        green: greenbeanie
+    };
 
     //appears after 300px scroll
     const toggleVisibility = () => {
@@ -34,14 +45,15 @@ const StickyFooter = () => {
     return (
         <div className={`sticky-footer ${isVisible ? 'show' : ''}`}>
           <div className="content">
-            <div>The Play Cool Beanie</div>
+                <img src={beanieImages[style] || '/img/footerimages/redbeanie.webp'} alt="Beanie" />
+                <div>The Play Cool Beanie</div>
           </div>
             <div className="custom-select">
                 <select value={style} onChange={e => setStyle(e.target.value)}>
                     <option value="red">Red - $25.00</option>
                     <option value="yellow">Yellow - $25.00</option>
                     <option value="blue">Blue - $25.00</option>
-                    <option value="green">Green</option>
+                    <option value="green">Green - $25.00</option>
                 </select>
             </div>
             <div className="actions">
